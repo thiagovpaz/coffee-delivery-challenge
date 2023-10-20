2e84ab4ded4dc7c6548f2136c984f010e23ee8c4
thiagovpaz <programador@programador.me>
2023-10-20 11:05:44 +0900

    feat: add custom fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import type { Metadata } from 'next';
-import { Roboto } from 'next/font/google';
+import { Baloo_2, Roboto } from 'next/font/google';
 
 import './globals.css';
 
-const roboto = Roboto({ weight: ['100', '400'], subsets: ['latin'] });
+const roboto = Roboto({
+  weight: ['100', '400'],
+  subsets: ['latin'],
+  variable: '--font-roboto',
+});
+const baloo = Baloo_2({
+  weight: ['400', '600', '800'],
+  subsets: ['latin'],
+  variable: '--font-baloo_2',
+});
 
 export const metadata: Metadata = {
   title: 'Coffee Delivery',
@@ -19,7 +28,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt">
-      <body className={roboto.className}>{children}</body>
+      <body
+        className={`${roboto.variable} ${baloo.variable} ${roboto.className}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: ["./src/**/*.tsx"],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          900: '#C47F17',
+          500: '#DBAC2C',
+          100: '#F1E9C9',
+        },
+        purple: {
+          900: '#4B2995',
+          500: '#8047F8',
+          100: '#EBE5F9',
+        },
+        gray: {
+          900: '#272221',
+          800: '#403937',
+          700: '#574F4D',
+          600: '#8D8686',
+          500: '#D7D5D5',
+          400: '#E6E5E5',
+          300: '#EDEDED',
+          200: '#F3F2F2',
+          100: '#FAFAFA',
+        },
+      },
+      fontFamily: {
+        roboto: ['var(--font-roboto)'],
+        baloo_2: ['var(--font-baloo_2)'],
+      },
+    },
   },
   plugins: [],
 };
